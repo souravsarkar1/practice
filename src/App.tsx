79d8822fcfa5d49fd6cd3adde12bceab4bde9aad
Sourav Sarkar <souravsantipur9635@gmail.com>
2024-11-14 22:25:23 +0530

    ok

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="mx-auto text-center">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
